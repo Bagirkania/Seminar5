b7c152658b9083174f0bfb530a000b802f8d493c
Bagirka <bruna07@mail.ru>
2022-10-04 23:30:22 +0500

    Task01

---
+++ Sem5/Task01/Program.cs
@@ -0,0 +1,34 @@
+﻿
+void FillArray(int [] num )
+{
+ for (int i = 0; i < num.Length; i++)
+{
+    num[i] = new Random().Next(100 , 1000);
+}
+}
+    
+void PrintArray(int[] num )
+{
+for (int i = 0; i < num.Length; i++)
+{
+    Console.Write(num[i] + " ");  
+}
+    Console.WriteLine();
+}
+void PrintChet(int [] num)
+{
+    int count = 0;
+    for (int i = 0; i < num.Length; i++)
+{
+    if (num[i] % 2 == 0)
+    {
+      count ++;
+
+    }
+}
+Console.WriteLine("Количество Четных чисел в массиве : " + count);
+}
+int [] numbers =  new int [5];
+ FillArray(numbers);
+ PrintArray(numbers);
+ PrintChet(numbers);
---
+++ Sem5/Task02/Program.cs
@@ -0,0 +1,32 @@
+﻿
+void FillArray(int [] num )
+{
+ for (int i = 0; i < num.Length; i++)
+{
+    num[i] = new Random().Next(100 , 1000);
+}
+}
+    
+void PrintArray(int[] num )
+{
+for (int i = 0; i < num.Length; i++)
+{
+    Console.Write(num[i] + " ");  
+}
+    Console.WriteLine();
+}
+int SumNechetIndex(int [] num)
+{
+    int count = 0;
+    int sum = 0;
+    for (int i = 0; i < num.Length; i++)
+{
+    if (num[i] % 2 == 0) count ++;
+    if (i % 2 == 1) sum += num[i];  
+}
+return sum;
+}
+int [] numbers =  new int [5];
+ FillArray(numbers);
+ PrintArray(numbers);
+Console.WriteLine("Сумма элементов на нечетных позициях : " + SumNechetIndex(numbers));
